1598e0265c672d665832e342d3d536be659f6125
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2025-07-06 14:34:40 +0300

    fix rewrite

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    console.log("API_URL:", process.env.NEXT_PUBLIC_API_URL);
     return [
       {
         source: "/api/:path*",
-        destination:
-          process.env.NEXT_PUBLIC_API_URL || "http://localhost:4200/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
